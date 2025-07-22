edificeio/entcore

feat: #PEDAGO-2694, get user details by id using broker

broker-parent/broker-client/nest/src/nats/types.ts
@@ -1,419 +1,391 @@
 export interface UpsertGroupSharesRequestDTO {
-  currentUserId?: string;
-  groupId?: string;
-  permissions?: string[];
-  resourceId?: string;
-  application?: string;
+    currentUserId?: string;
+    groupId?: string;
+    permissions?: string[];
+    resourceId?: string;
+    application?: string;
 }
-
-
 export interface UpsertGroupSharesResponseDTO {
-  shares?: SharesResponseDTO[];
+    shares?: SharesResponseDTO[];
 }
 export interface SharesResponseDTO {
-  id?: string;
-  kind?: Kind;
-  permissions?: string[];
+    id?: string;
+    kind?: Kind;
+    permissions?: string[];
 }
-export interface Kind {}
-
+export interface Kind {
+}
 export interface RemoveGroupSharesRequestDTO {
-  currentUserId?: string;
-  groupId?: string;
-  groupExternalId?: string;
-  resourceId?: string;
-  application?: string;
+    currentUserId?: string;
+    groupId?: string;
+    groupExternalId?: string;
+    resourceId?: string;
+    application?: string;
 }
-
-
 export interface RemoveGroupSharesResponseDTO {
-  shares?: SharesResponseDTO[];
+    shares?: SharesResponseDTO[];
 }
 export interface SharesResponseDTO {
-  id?: string;
-  kind?: Kind;
-  permissions?: string[];
+    id?: string;
+    kind?: Kind;
+    permissions?: string[];
 }
-export interface Kind {}
-
+export interface Kind {
+}
 export interface FindSessionRequestDTO {
-  sessionId?: string;
-  cookies?: string;
-  headers?: {
-    [k: string]: string;
-  };
-  params?: {
-    [k: string]: string;
-  };
-  pathPrefix?: string;
-  path?: string;
+    sessionId?: string;
+    cookies?: string;
+    headers?: {
+        [k: string]: string;
+    };
+    params?: {
+        [k: string]: string;
+    };
+    pathPrefix?: string;
+    path?: string;
 }
-
-
 export interface FindSessionResponseDTO {
-  session?: SessionDto;
+    session?: SessionDto;
 }
 export interface SessionDto {
-  userId?: string;
-  externalId?: string;
-  firstName?: string;
-  lastName?: string;
-  username?: string;
-  birthDate?: string;
-  level?: string;
-  type?: string;
-  login?: string;
-  email?: string;
-  mobile?: string;
-  authorizedActions?: ActionDto[];
-  classes?: ClassDto[];
-  groups?: GroupDto[];
-  structures?: StructureDto[];
-  functions?: UserFunctionDto[];
-  superAdmin?: boolean;
+    userId?: string;
+    externalId?: string;
+    firstName?: string;
+    lastName?: string;
+    username?: string;
+    birthDate?: string;
+    level?: string;
+    type?: string;
+    login?: string;
+    email?: string;
+    mobile?: string;
+    authorizedActions?: ActionDto[];
+    classes?: ClassDto[];
+    groups?: GroupDto[];
+    structures?: StructureDto[];
+    functions?: UserFunctionDto[];
+    superAdmin?: boolean;
 }
 export interface ActionDto {
-  type?: string;
-  name?: string;
-  displayName?: string;
+    type?: string;
+    name?: string;
+    displayName?: string;
 }
 export interface ClassDto {
-  id?: string;
-  name?: string;
+    id?: string;
+    name?: string;
 }
 export interface GroupDto {
-  id?: string;
-  name?: string;
+    id?: string;
+    name?: string;
 }
 export interface StructureDto {
-  id?: string;
-  name?: string;
+    id?: string;
+    name?: string;
 }
 export interface UserFunctionDto {
-  scope?: string[];
-  code?: string;
+    scope?: string[];
+    code?: string;
 }
-
 export interface CreateGroupRequestDTO {
-  externalId?: string;
-  name?: string;
-  classId?: string;
-  structureId?: string;
+    externalId?: string;
+    name?: string;
+    classId?: string;
+    structureId?: string;
 }
-
-
 export interface CreateGroupResponseDTO {
-  id?: string;
+    id?: string;
 }
-
 export interface UpdateGroupRequestDTO {
-  id?: string;
-  externalId?: string;
-  name?: string;
+    id?: string;
+    externalId?: string;
+    name?: string;
 }
-
-
 export interface UpdateGroupResponseDTO {
-  updated?: boolean;
+    updated?: boolean;
 }
-
 export interface DeleteGroupRequestDTO {
-  id?: string;
-  externalId?: string;
+    id?: string;
+    externalId?: string;
 }
-
-
 export interface DeleteGroupResponseDTO {
-  deleted?: boolean;
+    deleted?: boolean;
 }
-
 export interface AddGroupMemberRequestDTO {
-  groupId?: string;
-  groupExternalId?: string;
-  userId?: string;
+    groupId?: string;
+    groupExternalId?: string;
+    userId?: string;
 }
-
-
 export interface AddGroupMemberResponseDTO {
-  added?: boolean;
+    added?: boolean;
 }
-
 export interface RemoveGroupMemberRequestDTO {
-  groupId?: string;
-  groupExternalId?: string;
-  userId?: string;
+    groupId?: string;
+    groupExternalId?: string;
+    userId?: string;
 }
-
-
 export interface RemoveGroupMemberResponseDTO {
-  removed?: boolean;
+    removed?: boolean;
 }
-
 export interface FindGroupByExternalIdRequestDTO {
-  externalId?: string;
+    externalId?: string;
 }
-
-
 export interface FindGroupByExternalIdResponseDTO {
-  group?: GroupDTO;
+    group?: GroupDTO;
 }
 export interface GroupDTO {
-  id?: string;
-  name?: string;
+    id?: string;
+    name?: string;
 }
-
 export interface GetUserDisplayNamesRequestDTO {
-  userIds?: string[];
+    userIds?: string[];
 }
-
-
 export interface GetUserDisplayNamesResponseDTO {
-  userDisplayNames?: {
-    [k: string]: string;
-  };
+    userDisplayNames?: {
+        [k: string]: string;
+    };
 }
-
+export interface GetUsersByIdsRequestDTO {
+    userIds?: string[];
+}
+export interface GetUsersByIdsResponseDTO {
+    users?: UserDTO[];
+}
+export interface UserDTO {
+    id?: string;
+    displayName?: string;
+    profiles?: string[];
+    functions?: {
+        [k: string]: string[];
+    };
+}
 export interface AppRegistrationRequestDTO {
-  application?: AppRegistrationDTO;
-  actions?: SecuredActionDTO[];
+    application?: AppRegistrationDTO;
+    actions?: SecuredActionDTO[];
 }
 export interface AppRegistrationDTO {
-  name?: string;
-  displayName?: string;
-  appType?: string;
-  icon?: string;
-  address?: string;
-  display?: boolean;
-  prefix?: string;
-  customProperties?: {
-    [k: string]: {};
-  };
+    name?: string;
+    displayName?: string;
+    appType?: string;
+    icon?: string;
+    address?: string;
+    display?: boolean;
+    prefix?: string;
+    customProperties?: {
+        [k: string]: {};
+    };
 }
 export interface SecuredActionDTO {
-  name?: string;
-  displayName?: string;
-  type?: string;
+    name?: string;
+    displayName?: string;
+    type?: string;
 }
-
-
 export interface AppRegistrationResponseDTO {
-  success?: boolean;
-  message?: string;
+    success?: boolean;
+    message?: string;
 }
-
 export interface AppRegistrationRequestDTO {
-  application?: AppRegistrationDTO;
-  actions?: SecuredActionDTO[];
+    application?: AppRegistrationDTO;
+    actions?: SecuredActionDTO[];
 }
 export interface AppRegistrationDTO {
-  name?: string;
-  displayName?: string;
-  appType?: string;
-  icon?: string;
-  address?: string;
-  display?: boolean;
-  prefix?: string;
-  customProperties?: {
-    [k: string]: {};
-  };
+    name?: string;
+    displayName?: string;
+    appType?: string;
+    icon?: string;
+    address?: string;
+    display?: boolean;
+    prefix?: string;
+    customProperties?: {
+        [k: string]: {};
+    };
 }
 export interface SecuredActionDTO {
-  name?: string;
-  displayName?: string;
-  type?: string;
+    name?: string;
+    displayName?: string;
+    type?: string;
 }
-
-
 export interface AppRegistrationResponseDTO {
-  success?: boolean;
-  message?: string;
+    success?: boolean;
+    message?: string;
 }
-
 export interface FetchTranslationsRequestDTO {
-  headers?: {
-    [k: string]: string;
-  };
-  langAndDomain?: LangAndDomain;
-  application?: string;
+    headers?: {
+        [k: string]: string;
+    };
+    langAndDomain?: LangAndDomain;
+    application?: string;
 }
 export interface LangAndDomain {
-  lang?: string;
-  domain?: string;
+    lang?: string;
+    domain?: string;
 }
-
-
 export interface FetchTranslationsResponseDTO {
-  translations?: {
-    [k: string]: string;
-  };
-}
-
-export interface RegisterTranslationFilesRequestDTO {
-  application?: string;
-  translationsByLanguage?: {
-    [k: string]: {
-      [k: string]: string;
+    translations?: {
+        [k: string]: string;
     };
-  };
 }
-
-
+export interface RegisterTranslationFilesRequestDTO {
+    application?: string;
+    translationsByLanguage?: {
+        [k: string]: {
+            [k: string]: string;
+        };
+    };
+}
 export interface RegisterTranslationFilesResponseDTO {
-  application?: string;
-  languagesCount?: number;
-  translationsCount?: number;
+    application?: string;
+    languagesCount?: number;
+    translationsCount?: number;
 }
-
 export interface GetResourcesRequestDTO {
-  resourceIds?: string[];
+    resourceIds?: string[];
 }
-
-
 export interface GetResourcesResponseDTO {
-  resources?: ResourceInfoDTO[];
+    resources?: ResourceInfoDTO[];
 }
 export interface ResourceInfoDTO {
-  id?: string;
-  title?: string;
-  description?: string;
-  thumbnail?: string;
-  authorName?: string;
-  authorId?: string;
-  creationDate?: Date;
-  modificationDate?: Date2;
+    id?: string;
+    title?: string;
+    description?: string;
+    thumbnail?: string;
+    authorName?: string;
+    authorId?: string;
+    creationDate?: Date;
+    modificationDate?: Date2;
 }
 export interface Date {
-  gcal?: BaseCalendar;
-  jcal?: BaseCalendar1;
-  fastTime?: number;
-  cdate?: Date1;
-  defaultCenturyStart?: number;
-  serialVersionUID?: number;
-  wtb?: string[];
-  ttb?: number[];
+    gcal?: BaseCalendar;
+    jcal?: BaseCalendar1;
+    fastTime?: number;
+    cdate?: Date1;
+    defaultCenturyStart?: number;
+    serialVersionUID?: number;
+    wtb?: string[];
+    ttb?: number[];
 }
 export interface BaseCalendar {
-  JANUARY?: number;
-  FEBRUARY?: number;
-  MARCH?: number;
-  APRIL?: number;
-  MAY?: number;
-  JUNE?: number;
-  JULY?: number;
-  AUGUST?: number;
-  SEPTEMBER?: number;
-  OCTOBER?: number;
-  NOVEMBER?: number;
-  DECEMBER?: number;
-  SUNDAY?: number;
-  MONDAY?: number;
-  TUESDAY?: number;
-  WEDNESDAY?: number;
-  THURSDAY?: number;
-  FRIDAY?: number;
-  SATURDAY?: number;
-  BASE_YEAR?: number;
-  FIXED_DATES?: number[];
-  DAYS_IN_MONTH?: number[];
-  ACCUMULATED_DAYS_IN_MONTH?: number[];
-  ACCUMULATED_DAYS_IN_MONTH_LEAP?: number[];
+    JANUARY?: number;
+    FEBRUARY?: number;
+    MARCH?: number;
+    APRIL?: number;
+    MAY?: number;
+    JUNE?: number;
+    JULY?: number;
+    AUGUST?: number;
+    SEPTEMBER?: number;
+    OCTOBER?: number;
+    NOVEMBER?: number;
+    DECEMBER?: number;
+    SUNDAY?: number;
+    MONDAY?: number;
+    TUESDAY?: number;
+    WEDNESDAY?: number;
+    THURSDAY?: number;
+    FRIDAY?: number;
+    SATURDAY?: number;
+    BASE_YEAR?: number;
+    FIXED_DATES?: number[];
+    DAYS_IN_MONTH?: number[];
+    ACCUMULATED_DAYS_IN_MONTH?: number[];
+    ACCUMULATED_DAYS_IN_MONTH_LEAP?: number[];
 }
 export interface BaseCalendar1 {
-  JANUARY?: number;
-  FEBRUARY?: number;
-  MARCH?: number;
-  APRIL?: number;
-  MAY?: number;
-  JUNE?: number;
-  JULY?: number;
-  AUGUST?: number;
-  SEPTEMBER?: number;
-  OCTOBER?: number;
-  NOVEMBER?: number;
-  DECEMBER?: number;
-  SUNDAY?: number;
-  MONDAY?: number;
-  TUESDAY?: number;
-  WEDNESDAY?: number;
-  THURSDAY?: number;
-  FRIDAY?: number;
-  SATURDAY?: number;
-  BASE_YEAR?: number;
-  FIXED_DATES?: number[];
-  DAYS_IN_MONTH?: number[];
-  ACCUMULATED_DAYS_IN_MONTH?: number[];
-  ACCUMULATED_DAYS_IN_MONTH_LEAP?: number[];
+    JANUARY?: number;
+    FEBRUARY?: number;
+    MARCH?: number;
+    APRIL?: number;
+    MAY?: number;
+    JUNE?: number;
+    JULY?: number;
+    AUGUST?: number;
+    SEPTEMBER?: number;
+    OCTOBER?: number;
+    NOVEMBER?: number;
+    DECEMBER?: number;
+    SUNDAY?: number;
+    MONDAY?: number;
+    TUESDAY?: number;
+    WEDNESDAY?: number;
+    THURSDAY?: number;
+    FRIDAY?: number;
+    SATURDAY?: number;
+    BASE_YEAR?: number;
+    FIXED_DATES?: number[];
+    DAYS_IN_MONTH?: number[];
+    ACCUMULATED_DAYS_IN_MONTH?: number[];
+    ACCUMULATED_DAYS_IN_MONTH_LEAP?: number[];
 }
 export interface Date1 {
-  cachedYear?: number;
-  cachedFixedDateJan1?: number;
-  cachedFixedDateNextJan1?: number;
+    cachedYear?: number;
+    cachedFixedDateJan1?: number;
+    cachedFixedDateNextJan1?: number;
 }
 export interface Date2 {
-  gcal?: BaseCalendar2;
-  jcal?: BaseCalendar3;
-  fastTime?: number;
-  cdate?: Date3;
-  defaultCenturyStart?: number;
-  serialVersionUID?: number;
-  wtb?: string[];
-  ttb?: number[];
+    gcal?: BaseCalendar2;
+    jcal?: BaseCalendar3;
+    fastTime?: number;
+    cdate?: Date3;
+    defaultCenturyStart?: number;
+    serialVersionUID?: number;
+    wtb?: string[];
+    ttb?: number[];
 }
 export interface BaseCalendar2 {
-  JANUARY?: number;
-  FEBRUARY?: number;
-  MARCH?: number;
-  APRIL?: number;
-  MAY?: number;
-  JUNE?: number;
-  JULY?: number;
-  AUGUST?: number;
-  SEPTEMBER?: number;
-  OCTOBER?: number;
-  NOVEMBER?: number;
-  DECEMBER?: number;
-  SUNDAY?: number;
-  MONDAY?: number;
-  TUESDAY?: number;
-  WEDNESDAY?: number;
-  THURSDAY?: number;
-  FRIDAY?: number;
-  SATURDAY?: number;
-  BASE_YEAR?: number;
-  FIXED_DATES?: number[];
-  DAYS_IN_MONTH?: number[];
-  ACCUMULATED_DAYS_IN_MONTH?: number[];
-  ACCUMULATED_DAYS_IN_MONTH_LEAP?: number[];
+    JANUARY?: number;
+    FEBRUARY?: number;
+    MARCH?: number;
+    APRIL?: number;
+    MAY?: number;
+    JUNE?: number;
+    JULY?: number;
+    AUGUST?: number;
+    SEPTEMBER?: number;
+    OCTOBER?: number;
+    NOVEMBER?: number;
+    DECEMBER?: number;
+    SUNDAY?: number;
+    MONDAY?: number;
+    TUESDAY?: number;
+    WEDNESDAY?: number;
+    THURSDAY?: number;
+    FRIDAY?: number;
+    SATURDAY?: number;
+    BASE_YEAR?: number;
+    FIXED_DATES?: number[];
+    DAYS_IN_MONTH?: number[];
+    ACCUMULATED_DAYS_IN_MONTH?: number[];
+    ACCUMULATED_DAYS_IN_MONTH_LEAP?: number[];
 }
 export interface BaseCalendar3 {
-  JANUARY?: number;
-  FEBRUARY?: number;
-  MARCH?: number;
-  APRIL?: number;
-  MAY?: number;
-  JUNE?: number;
-  JULY?: number;
-  AUGUST?: number;
-  SEPTEMBER?: number;
-  OCTOBER?: number;
-  NOVEMBER?: number;
-  DECEMBER?: number;
-  SUNDAY?: number;
-  MONDAY?: number;
-  TUESDAY?: number;
-  WEDNESDAY?: number;
-  THURSDAY?: number;
-  FRIDAY?: number;
-  SATURDAY?: number;
-  BASE_YEAR?: number;
-  FIXED_DATES?: number[];
-  DAYS_IN_MONTH?: number[];
-  ACCUMULATED_DAYS_IN_MONTH?: number[];
-  ACCUMULATED_DAYS_IN_MONTH_LEAP?: number[];
+    JANUARY?: number;
+    FEBRUARY?: number;
+    MARCH?: number;
+    APRIL?: number;
+    MAY?: number;
+    JUNE?: number;
+    JULY?: number;
+    AUGUST?: number;
+    SEPTEMBER?: number;
+    OCTOBER?: number;
+    NOVEMBER?: number;
+    DECEMBER?: number;
+    SUNDAY?: number;
+    MONDAY?: number;
+    TUESDAY?: number;
+    WEDNESDAY?: number;
+    THURSDAY?: number;
+    FRIDAY?: number;
+    SATURDAY?: number;
+    BASE_YEAR?: number;
+    FIXED_DATES?: number[];
+    DAYS_IN_MONTH?: number[];
+    ACCUMULATED_DAYS_IN_MONTH?: number[];
+    ACCUMULATED_DAYS_IN_MONTH_LEAP?: number[];
 }
 export interface Date3 {
-  cachedYear?: number;
-  cachedFixedDateJan1?: number;
-  cachedFixedDateNextJan1?: number;
+    cachedYear?: number;
+    cachedFixedDateJan1?: number;
+    cachedFixedDateNextJan1?: number;
 }
-
+//# sourceMappingURL=ent-nats-service.types.d.ts.map

open-ent/entcore-v2

broker-parent/broker-client/nest/src/nats/types.ts
@@ -1,8 +1,6 @@
-export interface CreateGroupRequestDTO {
-  classId?: string;
+export interface DeleteGroupRequestDTO {
   externalId?: string;
-  name?: string;
-  structureId?: string;
+  id?: string;
 }
 
 export interface GroupDTO {
@@ -10,19 +8,46 @@ export interface GroupDTO {
   name?: string;
 }
 
-export interface FindSessionRequestDTO {
-  cookies?: string;
-  headers?: { [key: string]: string };
-  params?: { [key: string]: string };
-  path?: string;
-  pathPrefix?: string;
-  sessionId?: string;
+export interface GetUsersByIdsRequestDTO {
+  userIds?: string[];
 }
 
-export interface DummyResponseDTO {
-  jobId?: string;
-  success?: boolean;
+export interface ListenOnlyDTO {
+  timestamp?: number;
   userId?: string;
+}
+
+export interface Kind {
+}
+
+export interface UpsertGroupSharesRequestDTO {
+  application?: string;
+  currentUserId?: string;
+  groupId?: string;
+  permissions?: string[];
+  resourceId?: string;
+}
+
+export interface CreateGroupResponseDTO {
+  id?: string;
+}
+
+export interface FindGroupByExternalIdRequestDTO {
+  externalId?: string;
+}
+
+export interface GroupDto {
+  id?: string;
+  name?: string;
+}
+
+export interface UserFunctionDto {
+  code?: string;
+  scope?: string[];
+}
+
+export interface RemoveGroupMemberResponseDTO {
+  removed?: boolean;
 }
 
 export interface AddGroupMemberResponseDTO {
@@ -33,8 +58,45 @@ export interface DeleteGroupResponseDTO {
   deleted?: boolean;
 }
 
+export interface LangAndDomain {
+  domain?: string;
+  lang?: string;
+}
+
+export interface ClassDto {
+  id?: string;
+  name?: string;
+}
+
 export interface GetUserDisplayNamesRequestDTO {
   userIds?: string[];
+}
+
+export interface UpdateGroupRequestDTO {
+  externalId?: string;
+  id?: string;
+  name?: string;
+}
+
+export interface CreateGroupRequestDTO {
+  classId?: string;
+  externalId?: string;
+  name?: string;
+  structureId?: string;
+}
+
+export interface FetchTranslationsResponseDTO {
+  translations?: { [key: string]: string };
+}
+
+export interface GetUserDisplayNamesResponseDTO {
+  userDisplayNames?: { [key: string]: string };
+}
+
+export interface DummyResponseDTO {
+  jobId?: string;
+  success?: boolean;
+  userId?: string;
 }
 
 export interface AppRegistrationDTO {
@@ -48,62 +110,50 @@ export interface AppRegistrationDTO {
   prefix?: string;
 }
 
-export interface RegisterTranslationFilesRequestDTO {
-  application?: string;
-  translationsByLanguage?: { [key: string]: { [key: string]: string } };
+export interface AppRegistrationResponseDTO {
+  message?: string;
+  success?: boolean;
 }
 
-export interface RemoveGroupMemberResponseDTO {
-  removed?: boolean;
+export interface RegisterTranslationFilesResponseDTO {
+  application?: string;
+  languagesCount?: number;
+  translationsCount?: number;
 }
 
 export interface UpsertGroupSharesResponseDTO {
   shares?: SharesResponseDTO[];
 }
 
-export interface FindGroupByExternalIdResponseDTO {
-  group?: GroupDTO;
-}
-
-export interface RemoveGroupMemberRequestDTO {
+export interface AddGroupMemberRequestDTO {
   groupExternalId?: string;
   groupId?: string;
   userId?: string;
 }
 
-export interface UpdateGroupResponseDTO {
-  updated?: boolean;
+export interface StructureDto {
+  id?: string;
+  name?: string;
 }
 
-export interface FetchTranslationsResponseDTO {
-  translations?: { [key: string]: string };
+export interface GetUsersByIdsResponseDTO {
+  users?: UserDTO[];
 }
 
-export interface UpsertGroupSharesRequestDTO {
+export interface UserDTO {
+  displayName?: string;
+  functions?: { [key: string]: string[] };
+  id?: string;
+  profile?: string;
+}
+
+export interface RegisterTranslationFilesRequestDTO {
   application?: string;
-  currentUserId?: string;
-  groupId?: string;
-  permissions?: string[];
-  resourceId?: string;
+  translationsByLanguage?: { [key: string]: { [key: string]: string } };
 }
 
-export interface FetchTranslationsRequestDTO {
-  application?: string;
-  headers?: { [key: string]: string };
-  langAndDomain?: LangAndDomain;
-}
-
-export interface LangAndDomain {
-  domain?: string;
-  lang?: string;
-}
-
-export interface FindGroupByExternalIdRequestDTO {
-  externalId?: string;
-}
-
-export interface FindSessionResponseDTO {
-  session?: SessionDto;
+export interface FindGroupByExternalIdResponseDTO {
+  group?: GroupDTO;
 }
 
 export interface SessionDto {
@@ -126,21 +176,20 @@ export interface SessionDto {
   username?: string;
 }
 
-export interface RemoveGroupSharesResponseDTO {
-  shares?: SharesResponseDTO[];
+export interface AppRegistrationRequestDTO {
+  actions?: SecuredActionDTO[];
+  application?: AppRegistrationDTO;
 }
 
-export interface GetUserDisplayNamesResponseDTO {
-  userDisplayNames?: { [key: string]: string };
+export interface SecuredActionDTO {
+  displayName?: string;
+  name?: string;
+  type?: string;
 }
 
-export interface ListenOnlyDTO {
-  timestamp?: number;
-  userId?: string;
-}
-
-export interface ListenAndAnswerDTO {
-  jobId?: string;
+export interface RemoveGroupMemberRequestDTO {
+  groupExternalId?: string;
+  groupId?: string;
   userId?: string;
 }
 
@@ -152,39 +201,46 @@ export interface RemoveGroupSharesRequestDTO {
   resourceId?: string;
 }
 
-export interface CreateGroupResponseDTO {
+export interface RemoveGroupSharesResponseDTO {
+  shares?: SharesResponseDTO[];
+}
+
+export interface SharesResponseDTO {
   id?: string;
+  kind?: Kind;
+  permissions?: string[];
 }
 
-export interface DeleteGroupRequestDTO {
-  externalId?: string;
-  id?: string;
+export interface FetchTranslationsRequestDTO {
+  application?: string;
+  headers?: { [key: string]: string };
+  langAndDomain?: LangAndDomain;
 }
 
-export interface AppRegistrationRequestDTO {
-  actions?: SecuredActionDTO[];
-  application?: AppRegistrationDTO;
+export interface FindSessionRequestDTO {
+  cookies?: string;
+  headers?: { [key: string]: string };
+  params?: { [key: string]: string };
+  path?: string;
+  pathPrefix?: string;
+  sessionId?: string;
 }
 
-export interface AppRegistrationResponseDTO {
-  message?: string;
-  success?: boolean;
+export interface FindSessionResponseDTO {
+  session?: SessionDto;
 }
 
-export interface UpdateGroupRequestDTO {
-  externalId?: string;
-  id?: string;
+export interface ActionDto {
+  displayName?: string;
   name?: string;
+  type?: string;
 }
 
-export interface AddGroupMemberRequestDTO {
-  groupExternalId?: string;
-  groupId?: string;
+export interface ListenAndAnswerDTO {
+  jobId?: string;
   userId?: string;
 }
 
-export interface RegisterTranslationFilesResponseDTO {
-  application?: string;
-  languagesCount?: number;
-  translationsCount?: number;
+export interface UpdateGroupResponseDTO {
+  updated?: boolean;
 }